9b38a718f0e5ac628a58deaee7ec6e490d875f50
Utsanjan <59719700+utsanjan@users.noreply.github.com>
2021-07-26 23:46:10 +0530

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBuT8KXl-lhoA32MytMlLgcyhd-evzGxOA",
-    authDomain: "social-media-app-d9005.firebaseapp.com",
-    databaseURL: "https://social-media-app-d9005.firebaseio.com",
-    projectId: "social-media-app-d9005",
-    storageBucket: "social-media-app-d9005.appspot.com",
-    messagingSenderId: "575131298459",
-    appId: "1:575131298459:web:28939e6e6d5422748bf43e"
+    apiKey: "AIzaSyAOdlPjBMxaGFgM8qb8bf1uhNdJgrsvfSk",
+    authDomain: "greenhouse-e7063.firebaseapp.com",
+    databaseURL: "https://greenhouse-e7063.firebaseio.com",
+    projectId: "greenhouse-e7063",
+    storageBucket: "greenhouse-e7063.appspot.com",
+    messagingSenderId: "334874729244",
+    appId: "1:334874729244:web:3d549a2e01f404050bb8d9",
+    measurementId: "G-C79G9Q0C5Z"
   }
 };
