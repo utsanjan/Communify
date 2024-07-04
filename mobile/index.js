56b1a10420638835d2932df5ec72f1a596843ff1
Utsanjan Maity <utsanjan@gmail.com>
2024-07-04 18:40:58 +0530

    initial update

---
+++ mobile/index.js
@@ -0,0 +1,6 @@
+const containerEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+const popupContainerEl = document.querySelector(".popup-container");
+const closeIconEl = document.querySelector(".close-icon");
+containerEl.classList.add("active");
+popupContainerEl.classList.remove("active");
