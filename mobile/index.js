a1db6a6c26cde263ba3b0d39c4703c43fd42de6d
Utsanjan Maity <utsanjan@gmail.com>
2024-07-04 22:27:37 +0530

    initial update

--- mobile/index.js
+++ mobile/index.js
@@ -2,5 +2,13 @@ const containerEl = document.querySelector(".container");
 const btnEl = document.querySelector(".btn");
 const popupContainerEl = document.querySelector(".popup-container");
 const closeIconEl = document.querySelector(".close-icon");
+const preloader = document.querySelector(".preloader");
+const preloaderDuration = 400;
 containerEl.classList.add("active");
 popupContainerEl.classList.remove("active");
+const hidePreloader = () => {
+  setTimeout(() => {
+      preloader.classList.add("hide");
+  }, preloaderDuration);
+}
+window.addEventListener("load", hidePreloader);
